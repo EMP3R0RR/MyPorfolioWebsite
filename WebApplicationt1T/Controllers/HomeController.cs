57d7b65e4a0d287e073407463fb291a495b0c140
ASUS <ASUS@DESKTOP-8PLRN23>
2025-07-19 16:37:08 +0600

    Add project files.

---
+++ WebApplicationt1T/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationt1T.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult AboutMe()
+        {
+            return View();
+        }
+
+        public ActionResult Education()
+        {
+          
+            return View();
+        }
+
+        public ActionResult Skills()
+        {
+            return View();
+        }
+
+        public ActionResult Projects()
+        {
+            return View();
+        }
+
+        public ActionResult Contacts()
+        {
+             return View();
+        }
+    }
+}
---
+++ WebApplicationt1T/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationt1T.MvcApplication" Language="C#" %>
